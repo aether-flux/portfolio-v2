6f3eb936866b826bb1396824b1e9e6c7383bed3f
aether-flux <work.amar05@yahoo.com>
2026-01-26 20:23:32 +0530

    feat: add favicon

--- app/layout.js
+++ app/layout.js
@@ -11,10 +11,53 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
+const siteUrl = process.env.SITE_URL || "https://amartya-dev.vercel.app";
+
+const structuredData = {
+  "@context": "http://schema.org",
+  "@type": "Person",
+  name: "Amartya Chowdhury",
+  jobTitle: "Web Developer",
+  url: siteUrl,
+  sameAs: [
+    "https://www.linkedin.com/in/amartya-chowdhury",
+    "https://github.com/xerctia",
+  ],
+};
+
 export const metadata = {
   title: "Amartya Chowdhury",
-  description: "Developer portfolio of Amartya Chowdhury",
+  description:
+    "Portfolio website of Amartya Chowdhury, a web developer in React and Next js, and Express. See all of Amartya's skills, works, projects, experience and other professional aspects.",
+  icons: {
+    icon: "/faviconPort.ico",
+  },
+  // openGraph: {
+  //   type: "website",
+  //   url: siteUrl,
+  //   title: "Amartya Chowdhury's Portfolio",
+  //   description: "Showcasing web development projects, skills, and more.",
+  //   images: [
+  //     {
+  //       url: `${siteUrl}/og-img.jpg`,
+  //       width: 1200,
+  //       height: 630,
+  //       alt: "My Portfolio Image",
+  //     },
+  //   ],
+  // },
+  // twitter: {
+  //   card: "summary_large_image",
+  //   title: "Amartya Chowdhury's Portfolio",
+  //   description: "Showcasing web development projects, skills, and more.",
+  //   image: `${siteUrl}/og-img.jpg`,
+  // },
 };
+
+// export const metadata = {
+//   title: "Amartya Chowdhury",
+//   description: "Developer portfolio of Amartya Chowdhury",
+// };
 
 export default function RootLayout({ children }) {
   return (
